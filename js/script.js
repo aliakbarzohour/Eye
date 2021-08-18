a27563588ebd656e45fab3fde687bf09309acace
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-08-18 20:17:17 +0430

    added java script codes .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // start coding
+document.querySelector("body").addEventListener("mousemove", eyeball);
+
+function eyeball() {
+    const eye = document.querySelectorAll(".eye");
+    eye.forEach(function(eye) {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientWidth / 2);
+
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotation = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform = "rotate(" + rotation + "deg)"
+    });
+}
